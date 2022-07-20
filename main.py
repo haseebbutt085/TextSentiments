afc53c4d61d424f89a42c893af7faa537d9bc14e
Abdul Haseeb <67502044+haseebbutt085@users.noreply.github.com>
2022-07-20 13:07:30 +0500

    add arguments

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import uvicorn
 
 app = FastAPI()
 @app.get("/")
-def read_root():
-    res = TextBlob("sentence is  good")
-    return res.polarity
+def read_root(msg):
+    comments=msg.split("#")
+    i=0
+    for com in comments:
+      res = TextBlob(com)
+      i=i+res.polarity
+    return i
 if __name__=="__main__":
     uvicorn.run(app, host="127.0.0.1",port=8000)     
 
